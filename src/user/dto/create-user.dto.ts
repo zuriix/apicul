8f783068de623559ddc5139cd4df5dcad074d5f7
Zuriel Alvarez <alvarezcastrozuriel@gmail.com>
2024-05-20 19:26:11 -0500

    clase 6 de mayo realizada y estoy al dia

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import {IsEmail, IsNotEmpty, MinLength, MaxLength} from "class-validator";
+
+export class CreateUserDto {
+    @IsEmail({},{message: 'Formato de email inválido'})
+    email:string;
+    
+    @MinLength(10, {message: "Mínimo 10 Caracteres"})
+
+    @MaxLength(15, {message: "Máximo 15 caracteres"})
+    firstName: string;
+    
+    @IsNotEmpty()
+    lastName: string;
+
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { User } from './entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  controllers: [UserController],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
+  controllers: [UserController],
 })
 export class UserModule {}
